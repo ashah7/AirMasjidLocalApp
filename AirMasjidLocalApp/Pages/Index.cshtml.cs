b89d0375ae85e5988d03a25567e0abd3621bea60
abid shah <ashah7@gmail.com>
2019-04-21 17:34:30 +0100

    v1 testing

--- AirMasjidLocalApp/Pages/Index.cshtml.cs
+++ AirMasjidLocalApp/Pages/Index.cshtml.cs
@@ -1,17 +1,114 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using Newtonsoft.Json;
 
 namespace AirMasjidLocalApp.Pages
 {
+
+    [IgnoreAntiforgeryToken]
+
+
     public class IndexModel : PageModel
     {
+
+        private readonly ILogger _logger;
+        
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+
+
+        }
+
+        string serial = "cat /proc/cpuinfo | grep Serial | awk '{print $3}'".Bash().Split(';').First();
+
+
         public void OnGet()
         {
 
+
         }
-    }
-}
+
+        public ContentResult OnPostUpdateViewModeAsync()
+        {
+            var viewmode = "";
+
+            {
+                MemoryStream stream = new MemoryStream();
+                Request.Body.CopyTo(stream);
+                stream.Position = 0;
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    string requestBody = reader.ReadToEnd();
+                    if (requestBody.Length > 0)
+                    {
+                        var obj = JsonConvert.DeserializeObject(requestBody);
+                        if (obj != null)
+                        {
+                            viewmode = obj.ToString();
+
+                        }
+                    }
+                }
+            }
+
+
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+          .SetBasePath(Directory.GetCurrentDirectory())
+          .AddJsonFile("appsettings.json")
+          .Build();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connectionString))
+                {
+
+
+                    using (MySqlCommand cmd = new MySqlCommand("updateviewmode", conn))
+                    {
+
+                        //  cmd.CommandText = "updateestablishment";
+
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        conn.Open();
+
+                        var serialno = serial;
+                        cmd.Parameters.AddWithValue("@serialno", serial);
+                        cmd.Parameters.AddWithValue("@viewmode", viewmode);
+
+
+                        cmd.ExecuteNonQuery();
+                        conn.Close();
+
+                        return Content("success");
+                    }
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                var Message = $"Failed ViewMode update " + ex.Message;
+                _logger.LogWarning(Message);
+
+                return Content("failed");
+
+
+
+
+
+            }
+
+        }
+        }
---
+++ AirMasjidLocalApp/linuxcmdhelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AirMasjidLocalApp
+{
+    public static class Linuxcmdhelper
+    {
+
+
+        public static string Bash(this string cmd)
+        {
+            var escapedArgs = cmd.Replace("\"", "\\\"");
+
+            var process = new Process()
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                   // FileName = "/bin/bash",
+                         FileName = "C:\\MediaToolkit\\ffmpeg.exe",
+                    Arguments = $"-c \"{escapedArgs}\"",
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true,
+
+                }
+            };
+            process.Start();
+            string result = process.StandardOutput.ReadToEnd();
+
+            process.WaitForExit();
+
+
+            return result.Trim() + ";" + process.ExitCode;
+            //return process;
+
+        }
+
+
+
+
+    }
+}
